9be7dd9b950a21d9e53e9af801738629ebd20dbb
nawseekhiya <araj80317@gmail.com>
2025-10-06 18:51:04 +0530

    chore(errorMiddleware): rename unused next param to _next to fix no-unused-vars lint error

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -1,12 +1,12 @@
 // Handle 404s
-export const notFound = (req, res, next) => {
+export const notFound = (req, res, _next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
-  next(error);
+  _next(error);
 };
 
 // Central error handler
-export const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, req, res, _next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
