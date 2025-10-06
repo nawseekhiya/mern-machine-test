61cd463bb0011153bff251297450f696f03abffb
nawseekhiya <araj80317@gmail.com>
2025-10-06 18:49:09 +0530

    chore(eslint): suppress unused _next param in error handler

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,13 @@ export default defineConfig([
       },
     },
     rules: {
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^[A-Z_]",
+          argsIgnorePattern: "^_",
+        },
+      ],
     },
   },
 ]);
